e30cd865122186e6887e956dd237f5c9fb19581f
Manish Sharma <badgotra.manish@gmail.com>
2023-08-17 19:15:51 +0530

    rewrite pointer's main.go

--- pointers/main.go
+++ pointers/main.go
@@ -5,19 +5,19 @@ import "fmt"
 var name string
 
 func main() {
-	empName := "Manish Sharma"
-	ename := &empName
+	empName := "Manish Sharma" // initilize and declare variable.
+	ename := &empName // initilize and declares memory address of empName.
 
 	fmt.Println(empName)
 
 	fmt.Print("Enter name you want to enter: ")
-	fmt.Scanln(&name)
+	fmt.Scanln(&name) // takes string and stores it in "name" variable
 
 	empNameUpdate(ename, name)
 
-	fmt.Println(*ename)
+	fmt.Println(*ename) //prints ename variable's value by adding * before variable name.
 }
 
 func empNameUpdate(ptrname *string, text string) {
-	*ptrname = text
+	*ptrname = text // takes text from user and updates it to pointer variable.
 }

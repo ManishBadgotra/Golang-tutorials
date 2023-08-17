071fa3572fe6d3d4ef9a490dc27cac75566297da
Manish Sharma <badgotra.manish@gmail.com>
2023-08-17 19:08:17 +0530

    write README.md for pointers

--- pointers/main.go
+++ pointers/main.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
+var name string
+
 func main() {
 	empName := "Manish Sharma"
 	ename := &empName
 
 	fmt.Println(empName)
 
-	empNameUpdate(ename)
+	fmt.Print("Enter name you want to enter: ")
+	fmt.Scanln(&name)
+
+	empNameUpdate(ename, name)
 
 	fmt.Println(*ename)
 }
 
-func empNameUpdate(ename *string) {
-	*ename = "Ankit Sharma"
+func empNameUpdate(ptrname *string, text string) {
+	*ptrname = text
 }

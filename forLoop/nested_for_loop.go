09b5c73faaa88b797b18cdc31d7bdc94a6537702
Manish Sharma <badgotra.manish@gmail.com>
2023-08-18 13:24:06 +0530

    edit in nested for loop file

--- forLoop/nested_for_loop.go
+++ forLoop/nested_for_loop.go
@@ -6,7 +6,7 @@ func NestedLoop() {
 
 	for i := 1; i <= 3; i++ {
 		for j := 3; j >= 1; j-- {
-			fmt.Println(i, j)
+			fmt.Println(i, j) // in this j will execute till its condition matched when i is 1 before incrementing i to 2.
 		}
 	}
 }

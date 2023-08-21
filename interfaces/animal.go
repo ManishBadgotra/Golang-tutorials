a1f58152ab1971756f84aa11914fd2a70afdef6b
Manish Sharma <badgotra.manish@gmail.com>
2023-08-21 14:14:42 +0530

    interface example

--- interfaces/animal.go
+++ interfaces/animal.go
@@ -1,1 +1,77 @@
 package interfaces
+
+import "fmt"
+
+type Walker interface {
+	Walking()
+	Running()
+}
+
+type Bio interface {
+	Bio()
+}
+
+type Flyer interface {
+	Flying()
+}
+
+type Animal struct {
+	Breed    string
+	Sound    string
+	Nature string
+	CanFly   bool
+	CanSwim  bool
+	CanWalk  bool
+}
+
+type Dog struct {
+	Animal
+	Name string
+	Legs int
+}
+
+type Cat struct {
+	Animal
+	Name string
+	Legs int
+}
+
+type Eagle struct {
+	Animal
+	Wings int
+	Legs  int
+}
+
+/*
+	Declaring functions
+*/
+
+func (d Dog) Walking() {
+	fmt.Printf("Walking...")
+}
+
+func (c Cat) Walking() {
+	fmt.Printf("Walking...")
+}
+
+func (d Dog) Running() {
+	fmt.Printf("Running on %v legs...", d.Legs)
+}
+
+func (c Cat) Running() {
+	fmt.Printf("Running on %v legs...", c.Legs)
+}
+
+func (e Eagle) Flying() {
+	fmt.Printf("Can fly with %v wings.", e.Wings)
+}
+
+func (e Eagle) Walking() {
+	fmt.Printf("Can walk on their %v legs...", e.Legs)
+}
+
+func (d Dog) Bio() {
+	fmt.Println("Name:", d.Name)
+	fmt.Println("Breed:", d.Breed)
+	fmt.Println("Sound:", d.Sound)
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"Go/interfaces"
-	"Go/structures"
 	"fmt"
 )
 
@@ -17,22 +16,41 @@ func main() {
 		Calling through Structs.
 	*/
 
-	emp1 := structures.Employee{
-		FirstName: "Manish",
-		LastName:  "Sharma",
-		Age:       25,
-	}
+	// emp1 := structures.Employee{
+	// 	FirstName: "Manish",
+	// 	LastName:  "Sharma",
+	// 	Age:       25,
+	// }
 
-	emp1.Fullname()
+	// emp1.Fullname()
 
 	/*
-		Calling through Interface.
+		Calling area_and_perimeter Interface.
 	*/
 
-	rect := interfaces.Rectangle{
-		Length:  20,
-		Breadth: 10,
+	// rect := interfaces.Rectangle{
+	// 	Length:  20,
+	// 	Breadth: 10,
+	// }
+
+	// fmt.Println("Rectangle:", rect.Parameter())
+
+	/*
+		Calling animal via Interfaces.
+	*/
+
+	var ketu = interfaces.Dog{
+		Animal: interfaces.Animal{
+			Breed:   "Canine",
+			Sound:   "Woof",
+			Nature:  "Domestic",
+			CanFly:  false,
+			CanSwim: true,
+			CanWalk: true,
+		},
+		Name: "Ketu",
+		Legs: 4,
 	}
 
-	fmt.Println("Rectangle:", rect.Parameter())
+	fmt.Println(ketu)
 }

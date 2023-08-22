58eaaffa9df487237600d90b330ccace3d2fd509
Manish Sharma <badgotra.manish@gmail.com>
2023-08-22 14:12:53 +0530

    managed directory

--- multiGreetings/getChar.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// returns Unicode code point of 0th index in string.
-func GetChar(str string, index int) rune {
-
-	return []rune(str)[index] // makes rune slice of str and picks only 0th index value from it.
-}
--- multiGreetings/multiGreetings.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func multiGreetings(slice []string) {
-	for _, name := range slice { // used ok comma syntax to ignore index output from Range (a)
-		fmt.Printf("Namaste, %v\n", name)
-	}
-}
